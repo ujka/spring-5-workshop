dc263ea6489ebff5c80e109721c6e394d728332a
123marko <marko.ivanovic.13@gmail.com>
2019-10-23 00:31:12 +0200

    add property dependency injection

--- src/main/java/com/space/solartravel/info/ApplicationInfo.java
+++ src/main/java/com/space/solartravel/info/ApplicationInfo.java
@@ -2,7 +2,7 @@ package com.space.solartravel.info;
 
 public class ApplicationInfo {
 
-    private Version version;
+    private Version version = new Version();
     private SystemType systemType;
     private SpringCreator springCreator;
 }
