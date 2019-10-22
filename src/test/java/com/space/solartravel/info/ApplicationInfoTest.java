3f2977c6e2ffe4d877f9175bc51eb7b771a9146d
123marko <marko.ivanovic.13@gmail.com>
2019-10-23 01:29:45 +0200

    adapt unit test to use spring dependency injection, add test configuration

--- src/test/java/com/space/solartravel/info/ApplicationInfoTest.java
+++ src/test/java/com/space/solartravel/info/ApplicationInfoTest.java
@@ -1,16 +1,22 @@
 package com.space.solartravel.info;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = InfoTestConfiguration.class)
 class ApplicationInfoTest {
 
+    @Autowired
     private ApplicationInfo testee;
 
     @Test
     void toString_allPropertiesDefined_printsAll() {
-        testee = new ApplicationInfo(new SpringCreator());
         testee.setSystemType(new SystemType());
         String expectedResult = "ApplicationInfo{version=0.0.1, systemType=demo, springCreator=Rod Johnson}";
         assertEquals(expectedResult, testee.toString());
@@ -18,7 +24,6 @@ class ApplicationInfoTest {
 
     @Test
     void toString_systemTypeNotSet_printsAllExpectSystemType() {
-        testee = new ApplicationInfo(new SpringCreator());
         String expectedResult = "ApplicationInfo{version=0.0.1, systemType=null, springCreator=Rod Johnson}";
         assertEquals(expectedResult, testee.toString());
     }
---
+++ src/test/java/com/space/solartravel/info/InfoTestConfiguration.java
@@ -0,0 +1,9 @@
+package com.space.solartravel.info;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.space.solartravel.info")
+public class InfoTestConfiguration {
+}
