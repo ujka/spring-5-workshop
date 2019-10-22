6fd72f4aef1017fd50a54e951d2389cb1d56639a
123marko <marko.ivanovic.13@gmail.com>
2019-10-23 00:57:31 +0200

    show application info on Spring boot app startup

--- src/main/java/com/space/solartravel/SolarTravelApplication.java
+++ src/main/java/com/space/solartravel/SolarTravelApplication.java
@@ -1,5 +1,8 @@
 package com.space.solartravel;
 
+import com.space.solartravel.info.ApplicationInfo;
+import com.space.solartravel.info.SpringCreator;
+import com.space.solartravel.info.SystemType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,9 @@ public class SolarTravelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolarTravelApplication.class, args);
+		ApplicationInfo applicationInfo = new ApplicationInfo(new SpringCreator());
+		applicationInfo.setSystemType(new SystemType());
+		System.out.println(applicationInfo);
 	}
 
 }
