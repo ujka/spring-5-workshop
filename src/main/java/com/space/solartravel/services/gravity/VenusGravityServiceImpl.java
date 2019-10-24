0c31b3cd13deae02341e7aaf25cfc810157765e0
123marko <marko.ivanovic.13@gmail.com>
2019-10-25 01:26:16 +0200

    add @Value for gravity property as double for Venus - externalized properties

--- src/main/java/com/space/solartravel/services/gravity/VenusGravityServiceImpl.java
+++ src/main/java/com/space/solartravel/services/gravity/VenusGravityServiceImpl.java
@@ -1,11 +1,13 @@
 package com.space.solartravel.services.gravity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VenusGravityServiceImpl implements GravityService {
 
-    private double surfaceGravity = 0.91;
+    @Value("${gravity.surface_gravity.venus}")
+    private double surfaceGravity;
 
     @Override
     public double calculateForceWeight(double weight) {
