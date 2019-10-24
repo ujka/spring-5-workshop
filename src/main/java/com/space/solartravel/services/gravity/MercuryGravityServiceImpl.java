3fd606087382c001648c3b377ae228766262ed9b
123marko <marko.ivanovic.13@gmail.com>
2019-10-24 23:26:52 +0200

    add @Value for gravity property as double for Mercury - externalized properties, adapt test to read application.properties file - fails to load context for other tests

--- src/main/java/com/space/solartravel/services/gravity/MercuryGravityServiceImpl.java
+++ src/main/java/com/space/solartravel/services/gravity/MercuryGravityServiceImpl.java
@@ -1,11 +1,13 @@
 package com.space.solartravel.services.gravity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MercuryGravityServiceImpl implements GravityService {
 
-    private double surfaceGravity = 0.38;
+    @Value("${gravity.surface_gravity.mercury}")
+    private double surfaceGravity;
 
     @Override
     public double calculateForceWeight(double weight) {
