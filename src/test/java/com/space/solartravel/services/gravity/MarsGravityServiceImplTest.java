c170f19e3ce271a5f7334f842cf28b54688c11ee
123marko <marko.ivanovic.13@gmail.com>
2019-10-24 23:31:13 +0200

    adapt other tests to read application.properties file because of application context loading

--- src/test/java/com/space/solartravel/services/gravity/MarsGravityServiceImplTest.java
+++ src/test/java/com/space/solartravel/services/gravity/MarsGravityServiceImplTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = GravityTestConfiguration.class)
+@TestPropertySource("classpath:application.properties")
 class MarsGravityServiceImplTest {
 
     @Autowired
