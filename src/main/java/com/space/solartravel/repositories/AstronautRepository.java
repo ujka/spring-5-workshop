358b0d2bb79a203474e78949c565476be8c0564c
123marko <marko.ivanovic.13@gmail.com>
2019-10-28 03:29:21 +0100

    enhance Astronaut repository with @Query implementation, cover with test

--- src/main/java/com/space/solartravel/repositories/AstronautRepository.java
+++ src/main/java/com/space/solartravel/repositories/AstronautRepository.java
@@ -1,7 +1,18 @@
 package com.space.solartravel.repositories;
 
 import com.space.solartravel.domain.Astronaut;
+import com.space.solartravel.domain.SpaceShip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AstronautRepository extends JpaRepository<Astronaut, Long> {
+
+    @Query("select a from Astronaut a where a.assignedSpaceShip = :spaceShip")
+    List<Astronaut> findByAssignedSpaceShip(@Param("spaceShip") SpaceShip spaceShip);
+
+    @Query("select a from Astronaut a where a.assignedSpaceShip is null")
+    List<Astronaut> findAllUnassigned();
 }
--- src/test/java/com/space/solartravel/repositories/SpaceShipAstronautRepositoriesTest.java
+++ src/test/java/com/space/solartravel/repositories/SpaceShipAstronautRepositoriesTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,10 +21,11 @@ public class SpaceShipAstronautRepositoriesTest {
 
     @Autowired
     private AstronautRepository astronautRepository;
+    private SpaceShip spaceShip;
 
     @BeforeEach
     public void setUpData(){
-        SpaceShip spaceShip = new SpaceShip();
+        spaceShip = new SpaceShip();
         spaceShip.setName("Anna");
         spaceShip.setCapacity(100);
         spaceShip.setFuelCapacity(10);
@@ -37,8 +40,24 @@ public class SpaceShipAstronautRepositoriesTest {
         astronaut.setAssignedSpaceShip(spaceShip);
         spaceShip.getAstronauts().add(astronaut);
 
+        Astronaut astronaut2 = new Astronaut();
+        astronaut2.setName("John Doe 2");
+        astronaut2.setAge(28);
+        astronaut2.setWeight(67.9);
+        astronaut2.setEmail("john.doe2@johndoe.moc");
+        astronaut2.setAssignedSpaceShip(spaceShip);
+        spaceShip.getAstronauts().add(astronaut2);
+
         SpaceShip savedSpaceShip = spaceShipRepository.save(spaceShip);
         this.savedSpaceShipId = savedSpaceShip.getId();
+
+        Astronaut astronaut3 = new Astronaut();
+        astronaut3.setName("John Doe 3");
+        astronaut3.setAge(32);
+        astronaut3.setWeight(93.2);
+        astronaut3.setEmail("john.doe3@johndoe.moc");
+
+        astronautRepository.save(astronaut3);
     }
 
     @Test
@@ -68,5 +87,24 @@ public class SpaceShipAstronautRepositoriesTest {
         spaceShipRepository.deleteById(savedSpaceShipId);
 
         assertNotNull(astronautRepository.getOne(astronaut1.getId()));
+    }
+
+    @Test
+    public void findBySpaceShip_hasAstronautsAssigned_returnsAllAssignedAstronauts() {
+        SpaceShip spaceShip = spaceShipRepository.getOne(savedSpaceShipId);
+        List<Astronaut> astronauts = astronautRepository.findByAssignedSpaceShip(spaceShip);
+        assertEquals(spaceShip.getAstronauts().size(), astronauts.size());
+    }
+
+    @Test
+    public void findAllUnassigned_hasUnassignedAstronauts_returnsAllUnassigned() {
+        List<Astronaut> unasignedAstronauts = astronautRepository.findAllUnassigned();
+        assertEquals(1, unasignedAstronauts.size());
+        Astronaut astronaut = unasignedAstronauts.iterator().next();
+        assertEquals("John Doe 3", astronaut.getName());
+        assertEquals(32, astronaut.getAge());
+        assertEquals(93.2, astronaut.getWeight());
+        assertEquals("john.doe3@johndoe.moc", astronaut.getEmail());
+
     }
 }
